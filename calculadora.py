805eb3dd3fda172d6c016d2876336eea9e0143e8
Martín <zx22student3221@colegiovalledelmiro.es>
2023-09-26 10:30:55 +0200

    Calculadora v1.1(sumar y restar)

--- calculadora.py
+++ calculadora.py
@@ -9,12 +9,22 @@ def resta(a, b):
 # Función principal para la calculadora
 def calculadora():
     print("Calculadora")
+    print("1. Suma")
+    print("2. Resta")
+
+    # Solicitar al usuario que seleccione una operación
+    opcion = input("Seleccione una operación (1/2): ")
 
     # Solicitar al usuario que ingrese dos números
     num1 = float(input("Ingrese el primer número: "))
     num2 = float(input("Ingrese el segundo número: "))
 
-    print("Resultado:", suma(num1, num2))
+    if opcion == '1':
+        print("Resultado:", suma(num1, num2))
+    elif opcion == '2':
+        print("Resultado:", resta(num1, num2))
+    else:
+        print("Opción no válida")
 
 # Ejecutar la calculadora
 calculadora()
